b50e9ac995f33292bff755143e2015e4c39aac21
Alex <aleksandrdorofeev04@gmail.com>
2024-06-28 12:16:31 +0500

    Rudik's Cube 1.5

--- Cube.gd
+++ Cube.gd
@@ -2,33 +2,61 @@ extends MeshInstance3D
 
 var CubePiecePref: PackedScene = load("res://Prefabs/CubePiece.tscn")
 var CubeAllPieces = []
+var RotationVectors = [Vector3(0, 1, 0), Vector3(0, -1, 0),
+					   Vector3(1, 0, 0), Vector3(-1, 0, 0),
+					   Vector3(0, 0, -1), Vector3(0, 0, 1)]
 var CubeCenterPiece
 var CanRotate = true
+var CanShuffle = true
 
 func _ready():
 	СreateСube()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
+	
 func _input(event):
-	if CanRotate:
+	if CanRotate and CanShuffle:
 		if event.is_action_pressed("RotateUpPieces"):
-			Rotate(GetUpPieces(), Vector3(0, 1, 0))
+			Rotate(GetUpPieces(), RotationVectors[0])
 		elif event.is_action_pressed("RotateDownPieces"):
-			Rotate(GetDownPieces(), Vector3(0, -1, 0))
+			Rotate(GetDownPieces(), RotationVectors[1])
 		elif event.is_action_pressed("RotateFrontPieces"):
-			Rotate(GetFrontPieces(), Vector3(1, 0, 0))
+			Rotate(GetFrontPieces(), RotationVectors[2])
 		elif event.is_action_pressed("RotateBackPieces"):
-			Rotate(GetBackPieces(), Vector3(-1, 0, 0))
+			Rotate(GetBackPieces(), RotationVectors[3])
 		elif event.is_action_pressed("RotateLeftPieces"):
-			Rotate(GetLeftPieces(), Vector3(0, 0, -1))
+			Rotate(GetLeftPieces(), RotationVectors[4])
 		elif event.is_action_pressed("RotateRightPieces"):
-			Rotate(GetRightPieces(), Vector3(0, 0, 1))
+			Rotate(GetRightPieces(), RotationVectors[5])
+		elif event.is_action_pressed("RotateUpPiecesNegative"):
+			Rotate(GetUpPieces(), RotationVectors[0], -1)
+		elif event.is_action_pressed("RotateDownPiecesNegative"):
+			Rotate(GetDownPieces(), RotationVectors[1], -1)
+		elif event.is_action_pressed("RotateFrontPiecesNegative"):
+			Rotate(GetFrontPieces(), RotationVectors[2], -1)
+		elif event.is_action_pressed("RotateBackPiecesNegative"):
+			Rotate(GetBackPieces(), RotationVectors[3], -1)
+		elif event.is_action_pressed("RotateLeftPiecesNegative"):
+			Rotate(GetLeftPieces(), RotationVectors[4], -1)
+		elif event.is_action_pressed("RotateRightPiecesNegative"):
+			Rotate(GetRightPieces(), RotationVectors[5], -1)
+		elif event.is_action_pressed("Shuffle"):
+			Shuffle()
+		elif event.is_action_pressed("Restore"):
+			СreateСube()
+			
 			
 # Функция создания куба
 func СreateСube():
+	for cube in CubeAllPieces:
+		cube.queue_free()
+
+	CubeAllPieces.clear()
+	
 	for x in range(3):
 		for y in range(3):
 			for z in range(3):
@@ -39,13 +67,18 @@ func СreateСube():
 				CubeAllPieces.append(instance)
 	CubeCenterPiece = CubeAllPieces[13]
 
-func Rotate(pieces, rotationVec, speed = 0.001):
+
+# direction - направление вращения (1 для по часовой, -1 для против часовой)
+func Rotate(pieces, rotationVec, direction = 1, rotateAngle = 5):
 	CanRotate = false
+	
 	var total_angle = 0
 	var center = CubeCenterPiece.transform.origin
 	
+	var angle = deg_to_rad(rotateAngle) * direction
+	
 	# Создаем Basis (матрица вращения)
-	var rotation_basis = Basis().rotated(rotationVec, deg_to_rad(5))
+	var rotation_basis = Basis().rotated(rotationVec, angle)
 	
 	while total_angle < 90:
 		for piece in pieces:
@@ -59,12 +92,36 @@ func Rotate(pieces, rotationVec, speed = 0.001):
 			piece.transform.origin = center + relative_position
 			
 			# Выполняем локальное вращение кубика вокруг его оси
-			piece.transform.basis = piece.transform.basis.rotated(rotationVec, deg_to_rad(5))
+			piece.transform.basis = piece.transform.basis.rotated(rotationVec, angle)
 		
-		total_angle += 5  # Увеличиваем общий угол для следующего шага
-		await get_tree().create_timer(speed).timeout
+		total_angle += rotateAngle  # Увеличиваем общий угол для следующего шага
+		await get_tree().create_timer(0.001).timeout
 
-	CanRotate = true  # Разрешаем другие вращения после завершения текущего
+	CanRotate = true
+
+
+func Shuffle():
+	CanShuffle = false
+
+	var move_count = randi() % 51 + 50
+	
+	for i in range(move_count):
+		var edge = randi() % 6
+		var direction = randi() % 2 * 2 - 1
+		var edge_pieces
+		
+		if edge == 0: edge_pieces = GetUpPieces()
+		elif edge == 1: edge_pieces = GetDownPieces()
+		elif edge == 2: edge_pieces = GetFrontPieces()
+		elif edge == 3: edge_pieces = GetBackPieces()
+		elif edge == 4: edge_pieces = GetLeftPieces()
+		elif edge == 5: edge_pieces = GetRightPieces()
+	
+		Rotate(edge_pieces, RotationVectors[edge], direction, 15)
+		await get_tree().create_timer(0.04).timeout
+		
+	CanShuffle = true
+
 
 # Возвращает все кубики на верхней плоскости (y == 0)
 func GetUpPieces():
@@ -74,6 +131,7 @@ func GetUpPieces():
 			result.append(cube)
 	return result
 	
+	
 # Возвращает все кубики на нижней плоскости (y == -2)
 func GetDownPieces():
 	var result = []
@@ -82,6 +140,7 @@ func GetDownPieces():
 			result.append(cube)
 	return result
 
+
 # Возвращает все кубики на левой плоскости (z == 0)
 func GetLeftPieces():
 	var result = []
@@ -89,6 +148,7 @@ func GetLeftPieces():
 		if round(cube.transform.origin.z) == 0:
 			result.append(cube)
 	return result
+
 
 # Возвращает все кубики на правой плоскости (z == -2)
 func GetRightPieces():
@@ -106,6 +166,7 @@ func GetFrontPieces():
 		if round(cube.transform.origin.x) == 0:
 			result.append(cube)
 	return result
+
 
 # Возвращает все кубики на задней плоскости (x == -2)
 func GetBackPieces():
