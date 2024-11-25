87a2ce071ae03b691c4ca65cdc859d8b3fafed08
FeIqqu <aleksandrdorofeev04@gmail.com>
2024-11-26 01:48:38 +0500

    Changed camera control

--- Camera.gd
+++ Camera.gd
@@ -10,8 +10,9 @@ const RAY_LENGTH = 100
 var azimuth: float = PI / 4
 var zenith: float = PI / 4
 
-# Флаг для отслеживания состояния нажатия кнопки мыши
-var mouse_pressed: bool = false
+# Флаг для отслеживания состояния нажатия кнопок мыши
+var lmb_mouse_pressed: bool = false
+var rmb_mouse_pressed: bool = false
 
 # Расстояние камеры от центра сцены
 var distance: float = 6.0
@@ -19,8 +20,6 @@ var distance: float = 6.0
 # Центр вращения
 var center_offset: Vector3 = Vector3(-1, -1, 1)
 
-var camera_disabled: bool = false
-var rotate_disabled: bool = false
 var pieces: Array[MeshInstance3D] = []
 var planes: Array[MeshInstance3D] = []
 @onready var cube: MeshInstance3D = $"../Cube"
@@ -31,36 +30,30 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			mouse_pressed = event.pressed
-
+			lmb_mouse_pressed = event.pressed
 			# Если ЛКМ нажата
-			if mouse_pressed:
+			if lmb_mouse_pressed:
 				process_mouse_click()
-
 			# Если ЛКМ отжата
 			else:
 				if pieces.size() == 2:
 					cube.detect_rotate(pieces, planes)
-					rotate_disabled = true
-
 				reset_selection()
+				
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
+			rmb_mouse_pressed = event.pressed
 
 	elif event is InputEventMouseMotion:
-		# Если ЛКМ зажата и происходит движения указателя мыши
-		if mouse_pressed:
-			# Происходит движение указателя мыши по кубику
-			if camera_disabled:
-				process_mouse_motion()
-			else:
-				rotate_camera(event)
+		if rmb_mouse_pressed:
+			rotate_camera(event)
+		elif lmb_mouse_pressed:
+			process_mouse_motion()
 
 # Обработка клика мыши
 func process_mouse_click():
 	var result = ray_cast()
 
 	if result:
-		camera_disabled = true;
-
 		var hit_object = result["collider"].get_parent()
 		var parent_object = hit_object.get_parent()
 
@@ -98,13 +91,9 @@ func ray_cast() -> Dictionary:
 func reset_selection():
 	pieces.clear()
 	planes.clear()
-	camera_disabled = false
-	rotate_disabled = false
 
 # Обработка вращения камеры
 func rotate_camera(event):
-	rotate_disabled = true
-
 	azimuth -= event.relative.x * ROTATION_SPEED
 	zenith -= event.relative.y * -ROTATION_SPEED
 
@@ -121,6 +110,6 @@ func update_camera_position() -> void:
 	var z = distance * cos(zenith) * cos(azimuth)
 
 	# Установка нового положения камеры
-	position = center_offset + Vector3(x, y, z)
+	global_transform.origin = center_offset + Vector3(x, y, z)
 
 	look_at(center_offset, Vector3(0, 1, 0))
